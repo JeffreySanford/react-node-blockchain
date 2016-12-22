59e15928d62935f07fef3eed845d42a047ba7a87
Jeffrey Sanford <jeffreysanford@gmail.com>
2016-12-22 00:03:40 -0700

    set dev notificaitions

--- container/kidsContainer.js
+++ container/kidsContainer.js
@@ -6,7 +6,7 @@ var Kid = React.createClass({
   },
   change: function(event){
     this.setState({value: event.target.value});
-    console.log(event.target.value);
+    console.log(this.props.name + ' chose '+ event.target.value);
   },
   render: function () {
     
