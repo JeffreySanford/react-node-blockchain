e5244c57d7190854e7e9b5679e914a571717aad2
Jeffrey Sanford <jeffreysanford@gmail.com>
2016-12-21 21:32:55 -0700

    fixed state in dropdown

--- container/kidsContainer.js
+++ container/kidsContainer.js
@@ -1,4 +1,13 @@
 var Kid = React.createClass({
+  getInitialState: function() {
+    return {
+      value: 'select'
+    }
+  },
+  change: function(event){
+    this.setState({value: event.target.value});
+    console.log(event.target.value);
+  },
   render: function () {
     
     return (
@@ -12,7 +21,14 @@ var Kid = React.createClass({
               <i>Their chosen alt coin currency is <b>{this.props.currency}</b></i>
               </div>
               <div id="kidRate" class="col-lg-4 col-md-12 col-sm-12 col-xs-12" name="kidRate" style={{ fontSize: '1.5em', padding: '0em' }}>
-                
+                      <div class="selectCurrencyState"></div>
+                        <select id='coinType' onChange={this.change} value={this.state.value} style={{	backgroundColor: '#88B04B', color: 'white', border: '1px solid white', padding: '3px' }}>
+                          <option value="ETH" name="Etherium">Etherium ( BTC-e )</option>
+                          <option value="LTE" name="Litecoin">Litecoin ( Polonium )</option>
+                          <option value="DSH" name="DASH">DASH ( BTC-e )</option>
+                          <option disabled value="ALT" name="Alt Coin">Alt Coin</option>
+                        </select>
+                        <p>{this.state.value}</p>
                 <p class="reportLowestTrade"  style={{ fontSize: '.8em' }}><span id="selCur">{this.props.currency}</span> has the best exchange rate currently</p>
                 <p class="reportPropsedTrade"  style={{ fontSize: '.8em' }}><i>20 BTC will trade for { 20 * 1.16 } ETH </i></p>
             </div>
